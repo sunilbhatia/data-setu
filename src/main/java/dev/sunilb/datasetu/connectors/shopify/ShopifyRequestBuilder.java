67e052ea1d84e39883ae48dd591cf93dc24f61f6
Sunil Bhatia <sunilbhatia79@gmail.com>
2021-10-14 21:56:10 +0530

    Added support for Shopify Query Argument

---
+++ src/main/java/dev/sunilb/datasetu/connectors/shopify/ShopifyRequestBuilder.java
@@ -0,0 +1,92 @@
+package dev.sunilb.datasetu.connectors.shopify;
+
+import dev.sunilb.datasetu.exceptions.DataSetuException;
+
+public class ShopifyRequestBuilder {
+
+    private String queryRoot;
+    private String scanFrom;
+    private long scanRows;
+    private String fields;
+    private String queryString;
+
+    private ShopifyRequestBuilder() {
+        this.scanRows = -1;
+    }
+
+    public static ShopifyRequestBuilder Builder() {
+        ShopifyRequestBuilder builder = new ShopifyRequestBuilder();
+        return builder;
+    }
+
+    public ShopifyRequestBuilder withQueryRoot(String queryRoot) {
+        this.queryRoot = queryRoot;
+        return this;
+    }
+
+    public ShopifyRequestBuilder build() {
+
+        if (queryRoot == null || scanFrom == null || fields == null)
+            throw new DataSetuException("Cannot build Shopify Request Object as mandatory objects are not instantiated");
+
+        return this;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder requestObject = new StringBuilder();
+        requestObject.append("{");
+        requestObject.append("\"query\":\"");
+        requestObject.append("{");
+        requestObject.append(this.queryRoot);
+        requestObject.append(this.getArguments());
+        requestObject.append(this.getEdgesNode());
+        requestObject.append("}\"");
+        requestObject.append("}");
+
+        return requestObject.toString();
+    }
+
+    private String getEdgesNode() {
+        return "{edges {node {" + fields + "}}}";
+    }
+
+    private String getArguments() {
+
+        StringBuilder arguments = new StringBuilder();
+
+        if (scanFrom.equals("first")) arguments.append("(first:" + scanRows);
+        else if (scanFrom.equals("last")) arguments.append("(last:" + scanRows);
+
+        if (queryString != null) {
+            arguments.append(",query:\\\"" + queryString + "\\\"");
+        }
+
+        arguments.append(")");
+
+        return arguments.toString();
+    }
+
+    public ShopifyRequestBuilder first(long scanRows) {
+        this.scanFrom = "first";
+        this.scanRows = scanRows;
+        return this;
+    }
+
+    public ShopifyRequestBuilder last(long scanRows) {
+        this.scanFrom = "last";
+        this.scanRows = scanRows;
+        return this;
+    }
+
+    public ShopifyRequestBuilder withFields(String fields) {
+        this.fields = fields;
+        return this;
+    }
+
+    public ShopifyRequestBuilder withQuery(String queryString) {
+        this.queryString = queryString;
+        return this;
+    }
+}
--- src/test/java/dev/sunilb/connectors/shopify/ShopifyRequestBuilderTest.java
+++ src/test/java/dev/sunilb/connectors/shopify/ShopifyRequestBuilderTest.java
@@ -1,2 +1,74 @@
-package dev.sunilb.connectors.shopify;public class ShopifyRequestBuilderTest {
+package dev.sunilb.connectors.shopify;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import dev.sunilb.datasetu.connectors.shopify.ShopifyRequestBuilder;
+
+import static org.testng.Assert.*;
+
+import dev.sunilb.datasetu.exceptions.DataSetuException;
+import dev.sunilb.helpers.TestHelpers;
+import org.testng.annotations.Test;
+
+public class ShopifyRequestBuilderTest {
+
+    @Test
+    public void shouldBuildShopifyGraphQLRequestBasic() throws JsonProcessingException {
+
+        ShopifyRequestBuilder shopifyRequestBuilder = ShopifyRequestBuilder
+                .Builder()
+                .withQueryRoot("orders")
+                .first(100)
+                .withFields("id name discountCode")
+                .build();
+        String requestJson = shopifyRequestBuilder.toString();
+        assertTrue(TestHelpers.isJsonEqual(requestJson, "{\"query\": \"{orders(first:100){edges {node {id name discountCode}}}}\"}"));
+    }
+
+    @Test
+    public void shouldBuildShopifyGraphQLRequestWithQuery() throws JsonProcessingException {
+
+        ShopifyRequestBuilder shopifyRequestBuilder = ShopifyRequestBuilder
+                .Builder()
+                .withQueryRoot("orders")
+                .first(100)
+                .withFields("id name discountCode")
+                .withQuery("created_at:>2021-10-13")
+                .build();
+        String requestJson = shopifyRequestBuilder.toString();
+        assertTrue(TestHelpers.isJsonEqual(requestJson, "{\"query\": \"{orders(first:100,query:\\\"created_at:>2021-10-13\\\"){edges {node {id name discountCode}}}}\"}"));
+    }
+
+    @Test(expectedExceptions = DataSetuException.class)
+    public void showThrowExceptionWhenQueryRootIsNotSet() throws JsonProcessingException {
+
+        ShopifyRequestBuilder shopifyRequestBuilder = ShopifyRequestBuilder
+                .Builder()
+                .first(100)
+                .withFields("id name discountCode")
+                .withQuery("created_at:>2021-10-13")
+                .build();
+    }
+
+    @Test(expectedExceptions = DataSetuException.class)
+    public void showThrowExceptionWhenFirstLastIsNotSet() throws JsonProcessingException {
+
+        ShopifyRequestBuilder shopifyRequestBuilder = ShopifyRequestBuilder
+                .Builder()
+                .withQueryRoot("orders")
+                .withFields("id name discountCode")
+                .withQuery("created_at:>2021-10-13")
+                .build();
+    }
+
+    @Test(expectedExceptions = DataSetuException.class)
+    public void showThrowExceptionWhenFieldIsNotSet() throws JsonProcessingException {
+
+        ShopifyRequestBuilder shopifyRequestBuilder = ShopifyRequestBuilder
+                .Builder()
+                .withQueryRoot("orders")
+                .first(100)
+                .withQuery("created_at:>2021-10-13")
+                .build();
+    }
+
 }
